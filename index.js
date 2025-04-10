ed1aef8052d191d06f3e8b1e14dc14c393574140
cse-f <verify.moneymind@gmail.com>
2025-04-11 01:49:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,17 @@
 
 10)Write a JavaScript console program to find sum of digits of a given number using async/await.
    LINK => curl -k https://cse-f.github.io/fsd/ten.js
+
+11.	Write NodeJS program to implement the following using fs module:
+    1. Deleting file
+    2. Creating and Deleting directory
+    3. Deleting file within a directory
+    4. Removing the file and then removing the directory
+    5. creating a directory and writing the data to the file within the directory
+    6. Create a server that listens to client requests using http module
+   LINK => curl -k https://cse-f.github.io/fsd/eleven.js
+
+
    
    
   
