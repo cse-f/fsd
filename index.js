de6fb1c169bb4e079a087027e75df78cf5e641a2
cse-f <verify.moneymind@gmail.com>
2025-04-11 02:35:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,19 @@
   •	Double the values of each element in the array.
   •	Filter the doubled values to include only the numbers greater than 20.
   •	Return the resulting filtered array as output.
-  LINK => curl -k https://cse-f.github.io/fsd/second.js
+  LINK => curl -k https://cse-f.github.io/fsd/2.js
 
 3)Write a JavaScript program to calculate the sum of all elements in an array using an arrow function.
-  LINK => curl -k https://cse-f.github.io/fsd/third.js
+  LINK => curl -k https://cse-f.github.io/fsd/3.js
 
 4)Write a program to filter even numbers from an array using an arrow function.
-   LINK => curl -k https://cse-f.github.io/fsd/fourth.js
+   LINK => curl -k https://cse-f.github.io/fsd/4.js
   
 5)Write an arrow function to check if a given string is a palindrome
-   LINK => curl -k https://cse-f.github.io/fsd/fifth.js
+   LINK => curl -k https://cse-f.github.io/fsd/5.js
 
 6)Write a program to count the number of vowels in a string using an arrow function.
-   LINK => curl -k https://cse-f.github.io/fsd/sixth.js
+   LINK => curl -k https://cse-f.github.io/fsd/6.js
    
 7)Write a JavaScript program to implement
     •	Object Destructuring
@@ -22,16 +22,16 @@
     •	Destructuring with renaming
     •	Destructuring with function
     •	Destructuring with Rest operator
-   LINK => curl -k https://cse-f.github.io/fsd/seventh.js
+   LINK => curl -k https://cse-f.github.io/fsd/7.js
 
 8)Create 2 JavaScript files. Import one file into another and access a function to say Hello to user.
-   LINK => curl -k https://cse-f.github.io/fsd/eight.js
+   LINK => curl -k https://cse-f.github.io/fsd/8.js
 
 9)Write a JavaScript console program to find absolute value of a given number using async/await.
-   LINK => curl -k https://cse-f.github.io/fsd/nine.js
+   LINK => curl -k https://cse-f.github.io/fsd/9.js
 
 10)Write a JavaScript console program to find sum of digits of a given number using async/await.
-   LINK => curl -k https://cse-f.github.io/fsd/ten.js
+   LINK => curl -k https://cse-f.github.io/fsd/10.js
 
 11)Write NodeJS program to implement the following using fs module:
     1. Deleting file
@@ -40,10 +40,10 @@
     4. Removing the file and then removing the directory
     5. creating a directory and writing the data to the file within the directory
     6. Create a server that listens to client requests using http module
-   LINK => curl -k https://cse-f.github.io/fsd/eleven.js
+   LINK => curl -k https://cse-f.github.io/fsd/11.js
 
 12)Write NodeJS program to implement os, and http module.
-   LINK => curl -k https://cse-f.github.io/fsd/twelve.js
+   LINK => curl -k https://cse-f.github.io/fsd/12.js
 
 13)Write a node js program to demonstrate events 
    LINK => curl -k https://cse-f.github.io/fsd/13.js
