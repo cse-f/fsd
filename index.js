f51d399e3c2ecd03ab9ef74fe53b7f3d662013e6
cse-f <verify.moneymind@gmail.com>
2025-04-11 00:45:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-1)Write a JavaScript program that takes an array of integers as input. Use arrow functions to perform the following tasks:
+2)Write a JavaScript program that takes an array of integers as input. Use arrow functions to perform the following tasks:
   •	Double the values of each element in the array.
   •	Filter the doubled values to include only the numbers greater than 20.
   •	Return the resulting filtered array as output.
   LINK => curl -k https://cse-f.github.io/fsd/second.js
 
-2)Write a JavaScript program to calculate the sum of all elements in an array using an arrow function.
+3)Write a JavaScript program to calculate the sum of all elements in an array using an arrow function.
   LINK => curl -k https://cse-f.github.io/fsd/third.js
+
+4)Write a program to filter even numbers from an array using an arrow function.
+   LINK => curl -k https://cse-f.github.io/fsd/fourth.js
