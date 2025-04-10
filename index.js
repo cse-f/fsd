a1f7a1d6ab4c09c979805abdac5a7f3ff6a4d8b7
cse-f <verify.moneymind@gmail.com>
2025-04-11 00:41:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-this is index.js
+1)Write a JavaScript program that takes an array of integers as input. Use arrow functions to perform the following tasks:
+  •	Double the values of each element in the array.
+  •	Filter the doubled values to include only the numbers greater than 20.
+  •	Return the resulting filtered array as output.
+  LINK => https://cse-f.github.io/fsd/second.js
+
