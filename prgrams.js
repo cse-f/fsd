75b11bdc1cd1604161118461697f4ccf178c79be
cse-f <verify.moneymind@gmail.com>
2025-04-23 23:22:59 +0530

    Update prgrams.js

--- prgrams.js
+++ prgrams.js
@@ -1,48 +1,38 @@
-1. Create a git repository and clone it for the changes and publish the changes using git
-bash.
-2. Write a JavaScript program that takes an array of integers as input. Use arrow
-functions to perform the following tasks:
-   Double the values of each element in the array.
-   Filter the doubled values to include only the numbers greater than 20.
-   Return the resulting filtered array as output.
-3. Write a JavaScript program to calculate the sum of all elements in an array using an
-arrow function.
+1. Create a git repository and clone it for the changes and publish the changes using git bash.
+2. Write a JavaScript program that takes an array of integers as input. Use arrow functions to perform the following tasks:
+       Double the values of each element in the array.
+       Filter the doubled values to include only the numbers greater than 20.
+       Return the resulting filtered array as output.
+3. Write a JavaScript program to calculate the sum of all elements in an array using an arrow function.
 4. Write a program to filter even numbers from an array using an arrow function.
 5. Write an arrow function to check if a given string is a palindrome.
 6. Write a program to count the number of vowels in a string using an arrow function.
 7. Write a JavaScript program to implement
-   Object Destructuring
-   Array Destructuring
-   Destructuring with renaming
-   Destructuring with function
-   Destructuring with Rest operator
-8. Create 2 JavaScript files. Import one file into another and access a function to say
-Hello to user.
-9. Write a JavaScript console program to find absolute value of a given number using
-async/await.
-10. Write a JavaScript console program to find sum of digits of a given number using
-async/await.
+       Object Destructuring
+       Array Destructuring
+       Destructuring with renaming
+       Destructuring with function
+       Destructuring with Rest operator
+8. Create 2 JavaScript files. Import one file into another and access a function to sayHello to user.
+9. Write a JavaScript console program to find absolute value of a given number usingasync/await.
+10. Write a JavaScript console program to find sum of digits of a given number usingasync/await.
 11. Write NodeJS program to implement the following using fs module:
-1. Deleting file
-2. Creating and Deleting directory
-3. Deleting file within a directory
-4. Removing the file and then removing the directory
-5. creating a directory and writing the data to the file within the directory
-6. Create a server that listens to client requests using http module
+      1. Deleting file
+      2. Creating and Deleting directory
+      3. Deleting file within a directory
+      4. Removing the file and then removing the directory
+      5. creating a directory and writing the data to the file within the directory
+      6. Create a server that listens to client requests using http module
 12. Write NodeJS program to implement os, and http module.
 13. Write a node js program to demonstrate events
 14. Write a node js program to demonstrate read and write streams
 15. Write a node js program for reading and writing into file using pipes.
-16. Write a node js program for reading the file and sending the contents as response to
-the client request.
+16. Write a node js program for reading the file and sending the contents as response tothe client request.
 17. Write a node js program to serve the html page to the client.
 18. Write a node js program to serve the json page to the client
 19. Demonstrate Basic routing in node
-20. Create Express server listening requests at port 3000. Add different end points to
-provide access to the resources.
-21. Create a REST API for employees data and add different end points on express
-server to perform CRUD operations on the API. Test the end points using
-POSTMAN.
+20. Create Express server listening requests at port 3000. Add different end points toprovide access to the resources.
+21. Create a REST API for employees data and add different end points on express server to perform CRUD operations on the API. Test the end points using POSTMAN.
 22. Write an ejs program to display user information
 23. Write an ejs program to display contact information
 24. write a program to demonstrate function generators
@@ -51,34 +41,27 @@ POSTMAN.
 27. write a program to demonstrate closures
 28. write a js program to implement prototypal inheritance
 29. Use fetch function to access remote data using the given API
-   jsonplaceholder.typicode/users
-   Jsonplaceholder.typicode/todos
-   Restcountries.com
-30. Use fetch function to read the weather data from api “openweathermap.org” and
-display the details like min_temp,max_temp,avg_temp for a given city
-31. Read the weather forecast details for a given city and display details like date and
-temp in a table
-32. Create college database, and products collection and insert the following data in
-MongoDB
-   Find the products information whose price is greater than or equal to 200.
-   Find the products information whose price is less than 200.
-   Find the products information whose quantity is not equal to 300.
-   Find the products information whose price is equal to 200 and quantity is
-  equal to 60.
-   Find the products information whose price is equal to 500 or quantity is equal
-  to 60.
-   Find the products information whose price is not equal to 400 or quantity is
-  not equal to 40.
-   Find the products information whose price is either 200 or 400.
-   Find the products information whose price is neither 100 nor 500.
-   Update the price of laptop to 700.
-   Find the name, and price of the products whose _id is 5.
-   Sort the products collection based on the ascending order of name.
-   Sort the products collection based on the descending order of price and limit
-  the result to 2 documents.
-   Retrieve the name, and price of the product with the lowest price
-   Retrieve the third lowest-priced product, displaying only its name and price
-   Delete the product whose _id is 7.
+       jsonplaceholder.typicode/users
+       Jsonplaceholder.typicode/todos
+       Restcountries.com
+30. Use fetch function to read the weather data from api “openweathermap.org” anddisplay the details like min_temp,max_temp,avg_temp for a given city
+31. Read the weather forecast details for a given city and display details like date and temp in a table
+32. Create college database, and products collection and insert the following data in MongoDB
+       Find the products information whose price is greater than or equal to 200.
+       Find the products information whose price is less than 200.
+       Find the products information whose quantity is not equal to 300.
+       Find the products information whose price is equal to 200 and quantity is equal to 60.
+       Find the products information whose price is equal to 500 or quantity is equal to 60.
+       Find the products information whose price is not equal to 400 or quantity is not equal to 40.
+       Find the products information whose price is either 200 or 400.
+       Find the products information whose price is neither 100 nor 500.
+       Update the price of laptop to 700.
+       Find the name, and price of the products whose _id is 5.
+       Sort the products collection based on the ascending order of name.
+       Sort the products collection based on the descending order of price and limit  the result to 2 documents.
+       Retrieve the name, and price of the product with the lowest price
+       Retrieve the third lowest-priced product, displaying only its name and price
+       Delete the product whose _id is 7.
 33. Write an express application connecting mongodb and perform CRUD operations
 34. Create a real time database using firebase and perform CRUD operations
 35. create a firestore database in firebase and perform CRUD operations
